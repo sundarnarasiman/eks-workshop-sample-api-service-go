642a530985c6665ebf28090a9a1754b791b67fef
Sundararajan Narasiman <33112444+sundarnarasiman@users.noreply.github.com>
2022-06-02 17:52:41 +0530

    Update main.go

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world 1- the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
